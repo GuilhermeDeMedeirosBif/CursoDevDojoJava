45472b82ca9bf5f09d11fff23d081b9d4a9bd4f7
GuilhermeDeMedeirosBif <seuemail@example.com>
2025-05-12 22:06:15 -0400

    Tipos-primitivos-um-Java

---
+++ DevDojoJava/src/classes/Comentarios.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Comentarios {
+	// Comentário em uma linha
+	
+	/*
+	 * cometário de multiplas linhas
+	 * */
+	
+
+	
+}
---
+++ DevDojoJava/src/classes/TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		// int, double, float, char, byte, short, long, boolean
+		int age = 17;
+		System.out.println("A idade do Guilherme é "+age);
+	}
+}
