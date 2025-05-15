c6153cbf0ead3e2e6f15c6dc20cb8061f93f496c
GuilhermeDeMedeirosBif <seuemail@example.com>
2025-05-14 21:31:46 -0400

    Tipos-primitivos-Java

--- DevDojoJava/src/classes/TiposPrimitivos.java
+++ DevDojoJava/src/classes/TiposPrimitivos.java
@@ -4,6 +4,17 @@ public class TiposPrimitivos {
 	public static void main(String[] args) {
 		// int, double, float, char, byte, short, long, boolean
 		int age = 17;
+		long numeroGrande = 100000;
+		double salarioDouble = 2000;
+		float salarioFloat = 2500;
+		byte idadeByte = 10;
+		short idadeShort = 10;
+		boolean verdade  = true;
+		boolean falso = false;
+		char caractere = 'A'; 
+		
+		
 		System.out.println("A idade do Guilherme é "+age);
+		System.out.println(verdade);
 	}
 }
